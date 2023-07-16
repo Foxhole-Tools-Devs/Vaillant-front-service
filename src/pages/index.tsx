973bd0ebd59374236f97f50e73d17b423a05cbff
craftmusic <michel.stegmiller3@gmail.com>
2023-07-16 18:01:05 +0200

    Ajout endpoints discord et finalisation V1 discord

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import Layout from "@/modules/layout/layout";
 import Home from "@/modules/home/home";
 import { useSession } from "next-auth/react";
-import { session } from "next-auth/core/routes";
 
 export default function Index(){
   const  { data } = useSession();
